b5ed958a2db5446c2ba43b368464622c7df014a0
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-07-10 20:55:50 +0200

    eslint

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,4 +1,7 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require'lspconfig'.eslint.setup{
+  filetypes = { "typescript", "javascript" },
+}
 require'lspconfig'.tsserver.setup{
   filetypes = { "typescript", "javascript" },
   -- disableAutomaticTypingAcquisition = true,
