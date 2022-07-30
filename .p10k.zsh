b4d227d61545a25f9afcfce64427c9b380b9299e
Alex <alexanderekdahl@me.com>
2022-07-30 21:44:58 +0000

    linux icon

--- .p10k.zsh
+++ .p10k.zsh
@@ -189,7 +189,7 @@
   # OS identifier color.
   # typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=
   # Custom icon.
-   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🦒'
+   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🦁'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Green prompt symbol if the last command succeeded.
