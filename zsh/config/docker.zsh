3b9d6a2861872afd05b776ba981cfa3105bbf92b
AlexEkdahl <alexanderekdahl@me.com>
2023-08-29 10:11:06 +0200

    dbash

--- zsh/config/docker.zsh
+++ zsh/config/docker.zsh
@@ -17,8 +17,8 @@ function ds() {
 # Select a running docker container to enter shell
 function dbash() {
   local cid
-  cid=$(docker ps --format "{{ .ID }}\t{{ .Names }}\t{{ .Image }}" | sed 1d | fzf --print0 -m -1 --border=rounded --height 10% -q "$1" | awk '{print $1}')
+  cid=$(docker ps --format "{{ .ID }}\t{{ .Names }}\t{{ .Image }}" | fzf --print0 -m -1 --border=rounded --height 15% -q "$1" | awk '{print $1}')
 
-  [ -n "$cid" ] && docker exec -it "$cid" env TERM=xterm-256color bash
+  [ -n "$cid" ] && docker exec -it "$cid" env TERM=xterm-256color sh
 }
 
