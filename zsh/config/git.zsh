cf6045f05b6593d7d665c7e3f62f96d9ac3634e4
AlexEkdahl <alexanderekdahl@me.com>
2023-10-12 14:29:21 +0200

    git rebase alias

--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -12,6 +12,7 @@ alias gun='git --no-pager diff --name-only --cached | fzf --print0 -m -1 --borde
 alias gad='git ls-files -m -o --exclude-standard | fzf --print0 -m -1 --border=rounded --height 10% | xargs -0 -t -o git add'
 alias grd='git ls-files -m -o --exclude-standard | fzf --print0 -m -1 --border=rounded --height 10% | xargs -0 -t -o git restore'
 alias remotebranch="git for-each-ref --format='%(color:cyan)%(authordate:format:%m/%d/%Y %I:%M %p)    %(align:25,left)%(color:yellow)%(authorname)%(end) %(color:reset)%(refname:strip=3)' --sort=authordate refs/remotes"
+alias grebase="git fetch origin master:master && git rebase -i master"
 
 # Interactive git diff
 function gdiff {
