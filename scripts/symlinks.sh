b8c117c0df737646050249b586cb56d04047dac0
alexekdahl <77007088+alexekdahl@users.noreply.github.com>
2023-10-26 17:49:04 +0200

    script for setup

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
-# Specify the dotfiles and their corresponding paths
-dotfiles=(
-  "yabai/yabairc:~/.config/yabai/yabairc"
-  "skhd/skhdrc:~/.config/skhd/skhdrc"
-  "p10k/p10k.zsh:~/.p10k.zsh"
-  "zsh/.zshrc:~/.zshrc"
-  # "nvim/lua/:~/.config/nvim/lua/"
-  "nvim/init.vim:~/.config/nvim/init.vim"
-)
+nvim_lua_path="$HOME/.config/nvim/lua"
+nvim_ftplugin_path="$HOME/.config/nvim/after/ftplugin"
+nvim_go_path="$HOME/.config/nvim/after/ftplugin/go.vim"
+alacritty_path="$HOME/.config/alacritty"
+i3_path="$HOME/.config/i3"
+picom_path="$HOME/.config/picom"
+polybar_path="$HOME/.config/polybar"
+rofi_path="$HOME/.config/rofi"
 
-# Loop through each dotfile and create a symlink
-for dotfile in "${dotfiles[@]}"; do
-  src=$(echo "$dotfile" | cut -d':' -f1)
-  dest=$(echo "$dotfile" | cut -d':' -f2)
-  src_path="$HOME/.dotfiles/$src"
-  dest_path="$HOME/$dest"
+zshrc_link="$HOME/.dotfiles/zsh/.zshrc"
+p10k_link="$HOME/.dotfiles/p10k/p10k.zsh"
+nvim_link="$HOME/.dotfiles/nvim/init.vim"
 
-  # Check if the destination file already exists and is not a symlink
-  if [ -f "$dest_path" ] && [ ! -L "$dest_path" ]; then
-    echo "Backing up existing file at $dest_path..."
-    mv "$dest_path" "$dest_path.bak"
-  fi
+nvim_lua_link="$HOME/.dotfiles/nvim/lua/"
+alacritty_link="$HOME/.dotfiles/alacritty/"
+i3_link="$HOME/.dotfiles/i3/"
+picom_link="$HOME/.dotfiles/picom/"
+rofi_link="$HOME/.dotfiles/rofi/"
 
-  # Create the parent directory for the destination if it doesn't exist
-  dest_dir=$(dirname "$dest_path")
-  if [ ! -d "$dest_dir" ]; then
-    echo "Creating directory $dest_dir..."
-    mkdir -p "$dest_dir"
-  fi
+# Create necessary directories
+mkdir -p "$nvim_lua_path" "$nvim_ftplugin_path" "$alacritty_path" "$i3_path" "$picom_path" "$polybar_path" "$rofi_path"
 
-  # Create the symlink
-  echo "Creating symlink for $src_path at $dest_path..."
-  ln -sf "$src_path" "$dest_path"
-done
+# Backup function
+backup_if_exists() {
+    local target_file=$1
+    if [ -f "$target_file" ]; then
+        cp "$target_file" "$target_file.old"
+    fi
+}
+
+# Backup and symlink
+backup_if_exists "$HOME/.zshrc"
+ln -s "$zshrc_link" "$HOME/.zshrc"
+
+backup_if_exists "$HOME/.p10k.zsh"
+ln -s "$p10k_link" "$HOME/.p10k.zsh"
+
+backup_if_exists "$HOME/.config/nvim/init.vim"
+ln -s "$nvim_link" "$HOME/.config/nvim/init.vim"
+
+# Symlink entire folders
+ln -sf "$nvim_lua_link" "$nvim_lua_path"
+ln -sf "$alacritty_link" "$alacritty_path"
+ln -sf "$i3_link" "$i3_path"
+ln -sf "$picom_link" "$picom_path"
+ln -sf "$polybar_link" "$polybar_path"
+ln -sf "$rofi_link" "$rofi_path"
+
+# Add content to vim go file
+echo -e "setlocal shiftwidth=4\nsetlocal tabstop=4" > "$nvim_go_path"
