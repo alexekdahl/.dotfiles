8daf89a0d58e4b3d5e8552f54aab03d814f6a6f2
Alexander Ekdahl <alexanderekdahl@me.com>
2023-12-28 12:56:30 +0100

    remove old nvim + added 2 plugins

--- nvim.bak/lua/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-autopairs").setup({})
-require("nvim-treesitter.configs").setup({
-	highlight = { enable = true, additional_vim_regex_highlighting = false },
-	textobjects = { enable = true },
-	ensure_installed = { "javascript", "typescript", "go", "gomod", "gowork", "lua", "python", "bash", "vimdoc" },
-	indent = { enable = true },
-	autopairs = { enable = true },
-	rainbow = { enable = true },
-})
---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,11 @@
+return {
+	"ThePrimeagen/refactoring.nvim",
+	opts = {
+		prompt_func_return_type = {
+			go = true,
+		},
+		prompt_func_param_type = {
+			go = true,
+		},
+	},
+}
