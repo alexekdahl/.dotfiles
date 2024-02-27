150ea0d6d9885ddd9cb35df949ebc615ec5035a1
alexekdahl <alexanderekdahl@me.com>
2024-02-27 14:10:31 +0100

    refactor

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,90 +1,88 @@
-local config = function()
-	require("nvim-treesitter.configs").setup({
-		build = ":TSUpdate",
-		auto_install = true,
-		ensure_installed = {
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"bash",
-			"lua",
-			"dockerfile",
-			"gitignore",
-			"python",
-			"go",
-			"gomod",
-		},
-		event = { "BufReadPre", "BufNewFile" },
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		autopairs = {
-			enable = true,
-		},
-		rainbow = {
-			enable = true,
-		},
-		refactor = {
-			highlight_definitions = {
-				enable = true,
-			},
-			highlight_current_scope = {
-				enable = false,
-			},
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = "<c-s>",
-				node_decremental = "<M-space>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>ss"] = "@parameter.inner",
-				},
-			},
-		},
-		playground = { enable = false },
-	})
-end
-
 return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	config = config,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			build = ":TSUpdate",
+			auto_install = true,
+			ensure_installed = {
+				"markdown",
+				"json",
+				"javascript",
+				"typescript",
+				"yaml",
+				"html",
+				"bash",
+				"lua",
+				"dockerfile",
+				"gitignore",
+				"python",
+				"go",
+				"gomod",
+			},
+			event = { "BufReadPre", "BufNewFile" },
+			indent = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			autopairs = {
+				enable = true,
+			},
+			rainbow = {
+				enable = true,
+			},
+			refactor = {
+				highlight_definitions = {
+					enable = true,
+				},
+				highlight_current_scope = {
+					enable = false,
+				},
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>ss"] = "@parameter.inner",
+					},
+				},
+			},
+			playground = { enable = false },
+		})
+	end,
 }
--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,11 +1,11 @@
 return {
 	"Wansmer/treesj",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	keys = {
-		{ "<leader>m", "<cmd>TSJToggle<cr>" },
-	},
 	opts = {
 		use_default_keymaps = false,
 		max_join_length = 160,
+	},
+	keys = {
+		{ "<leader>m", "<cmd>TSJToggle<cr>" },
 	},
 }
