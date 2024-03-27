6224ba2e2a10a5b02d8006d9ee86f639560461d4
alexekdahl <alexanderekdahl@me.com>
2024-03-27 15:08:21 +0100

    gh copy to clipboard

--- nvim/lua/config/globals.lua
+++ nvim/lua/config/globals.lua
@@ -4,3 +4,4 @@ vim.g.editorconfig = true
 vim.g.gitblame_display_virtual_text = 0
 vim.g.gitblame_message_template = "<author> • <date> • <sha>"
 vim.g.gitblame_date_format = "%r"
+vim.g.gh_open_command = 'fn() { echo "$@" | xclip -selection clipboard; }; fn '
