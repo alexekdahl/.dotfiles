3da0236d3b67d03e4efb3392412e86f34ff86abc
alexekdahl <alexanderekdahl@me.com>
2024-05-20 11:22:56 +0200

    remove snippets - never use them

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,22 +2,14 @@ return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"onsails/lspkind.nvim",
-		{ "saadparwaiz1/cmp_luasnip", dependencies = { "L3MON4D3/LuaSnip" } },
-		"L3MON4D3/LuaSnip",
 	},
 	config = function()
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
 
 		vim.opt.completeopt = "menu,menuone,noselect"
 
 		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(),
 				["<C-j>"] = cmp.mapping.select_next_item(),
@@ -30,10 +22,6 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif luasnip.expandable() then
-						luasnip.expand()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
 					else
 						fallback()
 					end
@@ -44,8 +32,6 @@ return {
 				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
 					else
 						fallback()
 					end
@@ -57,7 +43,6 @@ return {
 			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
 			}),
@@ -68,7 +53,6 @@ return {
 						buffer = "[buf]",
 						nvim_lsp = "[LSP]",
 						path = "[path]",
-						luasnip = "[snip]",
 					},
 				}),
 			},
