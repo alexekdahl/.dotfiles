ea99eeba71703cecc3d8566d4fa6e301e9f8ab5e
alexekdahl <alexanderekdahl@me.com>
2025-02-12 10:43:30 +0100

    zsh

--- zsh/config/01-path.zsh
+++ zsh/config/01-path.zsh
@@ -1,11 +1,12 @@
-export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+
 export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar";
 export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew";
 export MANPATH="$HOMEBREW_PREFIX/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="$HOMEBREW_PREFIX/share/info:${INFOPATH:-}";
-export GOPATH="$HOME/.go"
-export NIMPATH="$HOME/.nim"
-export NIMBLEPATH="$HOME/.nimble"
+export GOPATH="$HOME/.local/go"
+export NIMPATH="$HOME/.local/nim"
+export NIMBLEPATH="$HOME/.local/nimble"
 export GOBIN="$GOPATH/bin"
 export NIMBIN="$NIMPATH/bin"
 export NIMBLE_BIN="$NIMBLEPATH/bin"
@@ -13,6 +14,7 @@ export NIMBLE_BIN="$NIMBLEPATH/bin"
 export PERSONAL="$HOME/dev/personal"
 export DOTFILES="$HOME/.dotfiles"
 export XDG_DATA_DIRS="$HOMEBREW_PREFIX/share:$XDG_DATA_DIRS"
+export XDG_CONFIG_HOME="$HOME/.config"
 
 # Path
 export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin${PATH+:$PATH}";
