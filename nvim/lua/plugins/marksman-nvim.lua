77625e9fd96bb531ec4bc9ccbc3bd812f9207236
alexekdahl <alexanderekdahl@me.com>
2025-10-28 13:27:51 +0100

    use my own plugin marksman

---
+++ nvim/lua/plugins/marksman-nvim.lua
@@ -0,0 +1,48 @@
+return {
+	"alexekdahl/marksman.nvim",
+	keys = {
+		{
+			"<C-a>",
+			function()
+				require("marksman").add_mark()
+			end,
+			desc = "Add mark",
+		},
+		{
+			"<C-e>",
+			function()
+				require("marksman").show_marks()
+			end,
+			desc = "Show marks",
+		},
+		{
+			"<M-y>",
+			function()
+				require("marksman").goto_mark(1)
+			end,
+			desc = "Go to mark 1",
+		},
+		{
+			"<M-u>",
+			function()
+				require("marksman").goto_mark(2)
+			end,
+			desc = "Go to mark 2",
+		},
+		{
+			"<M-i>",
+			function()
+				require("marksman").goto_mark(3)
+			end,
+			desc = "Go to mark 3",
+		},
+		{
+			"<M-o>",
+			function()
+				require("marksman").goto_mark(4)
+			end,
+			desc = "Go to mark 4",
+		},
+	},
+	opts = {},
+}
