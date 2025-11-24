d8ff3a6893a0b469c36e7a9d37c7ed1f5e8eb921
alexekdahl <alexanderekdahl@me.com>
2025-11-24 10:04:32 +0100

    setup

--- nvim/plugin/splitjoin.lua
+++ nvim/plugin/splitjoin.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command(
   'SplitJoinToggle',
-  function(_) require('splitjoin').toggle({}) end,
-  { nargs = 0, desc = 'Toggle split/join for bracketed collections' }
+  function() require('splitjoin').toggle({}) end,
+  { desc = 'Toggle split/join for bracketed collections' }
 )
